f08a1603023ea7ea70f685f95c0f19caebad6d2e
José Arias <j.ariascanto@gmail.com>
2022-10-16 10:18:52 -0500

    added python code for round1A double or one thing

---
+++ round1A/doubleoronething.py
@@ -0,0 +1,44 @@
+# By: José Arias
+#
+# Code Jam 2022 - Round 1A Double or One Thing
+# 
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+def test():
+    word = input()
+    
+    result = []
+    count = 1
+
+    for i in range(len(word)):
+        # checks if it's not the last character on  the string
+        if i+1 != len(word):
+            # adds 1 to count if the current character is the
+            # same as the next one
+            if word[i] == word[i+1]:
+                count += 1
+            # appends to the result the current character times 
+            # the value in the count variable
+            else:
+                if word[i] < word[i+1]:
+                    result.append(word[i] * count)
+                count = 1
+
+        # appends current character to our result list
+        result.append(word[i])
+
+    return "".join(result)
+
+
+if __name__ == '__main__':
+    result = []
+
+    # asks the user for a number of cases and appends the
+    # return of each test to our result list
+    for case in range(int(input())):
+        result.append(test())
+
+    # prints the result of each case
+    for case in range(len(result)):
+        print(f'Case #{case + 1}: {result[case]}')
